2432574d0e57c93b7ccb6718af73c4ea5bf8c0ae
andrewmahn <mahndrew@live.unc.edu>
2021-11-30 19:19:07 -0500

    Hopefully fixed patch

--- server.js
+++ server.js
@@ -42,8 +42,8 @@ app.get("/app/user/:id", (req, res) => {
 });
 // UPDATE a single user (HTTP method PATCH) at endpoint /app/update/user/:id
 app.patch("/app/update/user/:id", (req, res) => {
-	const stmt = db.prepare("UPDATE userinfo SET user = COALESCE(?, user), pass = COALESCE(?, pass) WHERE id = (?)").run(req.params.id);
-	res.status(405).json({"message": info.changes});
+	const stmt = db.prepare("UPDATE userinfo SET user = COALESCE(?, user), pass = COALESCE(?, pass) WHERE id = (?)").run(req.body.user, md5(req.body.pass));
+	res.status(200).json({"message": info.changes + " record updated: ID " + info.lastInsertRowid + " (200)"});
 });
 // DELETE a single user (HTTP method DELETE) at endpoint /app/delete/user/:id
 app.delete("/app/delete/user/:id", (req, res) => {
